e52312900ad08972535460291e388ee1057b48a2
koju <fine.fantasy.fanny@gmail.com>
2026-01-18 14:00:27 +0900

    update Main.java

--- SchoolApp/src/com/example/model/Teacher.java
+++ SchoolApp/src/com/example/model/Teacher.java
@@ -1,12 +1,16 @@
 package com.example.model;
 
 public class Teacher extends Person {
+    //フィールド
     private String subject;
+    
+    //コンストラクタ
     public Teacher(String name, int age, String subject){
         super(name, age);
         this.subject = subject;
     }
-
+    
+    //ゲッター
     public String getSubject(){
         return subject;
     }; 
